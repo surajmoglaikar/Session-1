93a41c0b2d40d1223ccad1d6480c23df3293c6d0
surajmoglaikar <surajmoglaikar64gmail.com>
2022-03-30 15:40:27 +0530

    this is second commit

--- src/app/calculator-component/calculator-component.component.ts
+++ src/app/calculator-component/calculator-component.component.ts
@@ -9,27 +9,25 @@ import { CalculatorServiceService } from '../calculator-service.service';
 export class CalculatorComponentComponent implements OnInit {
  public title ="CALCULATOR";
 
- public num1:number;
- public num2:number;
- public result:number;
+ 
 
-  constructor() { }
+  constructor(public calculatorServiceService : CalculatorServiceService) { }
 
   ngOnInit(): void {
   }
   public addition() {
-    this.result = this.num1 + this.num2;
+    this.calculatorServiceService.result = this.calculatorServiceService.num1 + this.calculatorServiceService.num2;
   }
 
   subtraction() {
-    this.result = this.num1 - this.num2;
+    this.calculatorServiceService.result = this.calculatorServiceService.num1 - this.calculatorServiceService.num2;
   }
 
   multiplication() {
-    this.result = this.num1 * this.num2;
+    this.calculatorServiceService.result = this.calculatorServiceService.num1 * this.calculatorServiceService.num2;
   }
 
   division() {
-    this.result = this.num1 / this.num2;
+    this.calculatorServiceService.result = this.calculatorServiceService.num1 / this.calculatorServiceService.num2;
   }
 }
